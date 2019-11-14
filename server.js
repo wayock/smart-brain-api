a1244a3d42f4e92420f3536c009bd98652f1f545
Matthew Wayock <wayock@gmail.com>
2019-11-13 22:32:43 -0500

    connect front and back ends

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
+const cors = require ('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -94,6 +96,6 @@ app.put('/image', (req, res) => {
 })
 
 app.listen(3001, () => {
-    console.log('app is running on port 3000')
+    console.log('app is running on port 3001')
 })
 
